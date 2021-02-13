31435321ed858f3d22e1ee6f0f56195ef7f3bece
Corey Stinson <cstinson@smalljoes.com>
2021-02-13 12:42:51 -0600

    Adds convenience build script.

---
+++ win32/build-x64-dyn-release.bat
@@ -0,0 +1,6 @@
+cscript configure.js compiler=msvc cruntime=/MD debug=no static=yes zlib=yes iconv=yes ^
+prefix=D:\Projects\lib\libxml2-2.9.2 ^
+include=D:\Projects\lib\libiconv-1.16\include;D:\Projects\lib\zlib-1.2.11\x64\release-dyn\include ^
+lib=D:\Projects\lib\libiconv-1.16\x64\release-dyn\lib;D:\Projects\lib\zlib-1.2.11\x64\release-dyn\lib ^
+incdir=D:\Projects\lib\libxml2-2.9.2\include ^
+libdir=D:\Projects\lib\libxml2-2.9.2\x64\release-dyn
